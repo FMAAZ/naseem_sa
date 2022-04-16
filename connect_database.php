4ef5e01f97501762a5fe05b7d76e51f43abf2f0e
Mohammed Abdullah Ali Alzahrani <75160309+FMAAZ@users.noreply.github.com>
2022-04-16 16:56:59 +0300

    create

---
+++ connect_database.php
@@ -0,0 +1,7 @@
+<?php
+        $hostName="localhost";
+        $user="root";
+        $pass="";
+        $db="naseem_sa";
+        $connect_database = new PDO("mysql:host=$hostName;dbname=$db;charset=utf8;",$user,$pass);
+?>

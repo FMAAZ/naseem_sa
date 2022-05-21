19c67075901ff051dec55b091e920a1f2933ef75
Mohammed Abdullah Ali Alzahrani <75160309+FMAAZ@users.noreply.github.com>
2022-05-21 10:07:20 +0300

    content edit

--- island.php
+++ island.php
@@ -1,53 +1,41 @@
 <!DOCTYPE html>
 <html lang="en" dir="rtl">
-<?php require('components/head_inc.php');?>  
+<?php require('components/head_inc.php'); $_SESSION["destination"] = "island";?>  
 <body>
-    <div id="myCarousel" class="carousel slide pointer-event" data-bs-ride="carousel">
+<div id="myCarousel" class="carousel slide pointer-event" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"
-                aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"
-                class="active"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active"
-                aria-current="true"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item">
-                <img src="assistances/images/islands.jpg" class="d-block w-100" alt="Cinque Terre" width="550px"
-                    height="555px">
 
-                <div class="container">
-                    <div class="carousel-caption text-end">
-                        <h1><span class=" text-white-50">رجال المع ...</span></h1>
-                        <p class="lead"><span class="text-decoration-underline">مزيج المراعي الخضراء والتراث الجنوبي الأصيل</span></p>
-                        <p><a class="btn btn-outline-light btn-lg" href="Cities.php">اعرض اكثر</a></p>
-                    </div>
+    <?php
+    require 'connect_database.php';
+    if($connect_database)
+    {
+        $select_photo = $connect_database->prepare('SELECT * FROM island_content');
+        $select_photo->execute();
+        foreach($select_photo as $print)
+        {
+            echo '
+                <div class="carousel-item active">
+                    <img src="assistances/images/'.$print["card_photo"].'" class="d-block w-100" alt="Cinque Terre" width="550px" height="555px">
+                        <div class="container">
+                            <div class="carousel-caption text-end">
+                                <h1><span class="text-white-50">'.$print["name"].'</span></h1>
+                                <p class="lead"><span class="text-decoration-underline">'.$print["card_description"].'</span></p>
+                                <form method="POST" action="try2.php">
+                                <button type="submit" name="card_island" class=" btn btn-outline-success" value="'.$print["ID"].'">عرض</button>
+                                </form>
+                            </div>
+                        </div>
                 </div>
-            </div>
-            <div class="carousel-item active">
-                <img src="assistances/images/islands.jpg" class="d-block w-100" alt="Cinque Terre" width="550px"
-                    height="555px">
+            ';
+        }
+    }
+    ?>
 
-                <div class="container">
-                    <div class="carousel-caption text-end">
-                        <h1><span class="text-white-50">جزيرة فرسان</span></h1>
-                        <p class="lead"><span class="text-decoration-underline">وجهة سياحية فريدة</span></p>
-                        <p class="lead"><a class="btn btn-outline-light btn-lg" href="Cities.php">اعرض اكثر</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="assistances/images/xz1.jpg" class="d-block w-100" alt="Cinque Terre" width="550px"
-                    height="555px">
-
-                <div class="container">
-                    <div class="carousel-caption text-end">
-                    <h2><span class="text-white-50">تعرفوا على أجمل جزر المملكة الخلابة...</span></h2>
-                        <p class="lead"><span class="text-decoration-underline">تحتوي المملكة العربية السعودية على المئات من الجزر المتنوعة والتي تطل على البحر الأحمر والخليج العربي وآلاف الجزر المهيئة للجميع من محبي ركوب الأمواج أو عشاق الغوص بين الشعب المرجانية أو عشاق المناظر الطبيعية للجزر البكر .</span></p>
-                            <p><a class="btn btn-outline-light btn-lg" href="island.php">اعرض اكثر</a></p>
-                    </div>
-                </div>
-            </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
             <span class="carousel-control-prev-icon bg-success" aria-hidden="true"></span>
@@ -57,64 +45,53 @@
             <span class="carousel-control-next-icon bg-success" aria-hidden="true"></span>
             <span class="visually-hidden">التالي</span>
         </button>
-</div>
-        <section class="bg-light py-4 my-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h2 id="scrollspyHeading2"></h2>
-            </div>
-
-                <div class="col-md-6 col-lg-4">
-                <div class="card my-3">
-                    <div class="card-thumbnail">
-                        <img src="assistances/images/islands.jpg" class="img-fluid" alt="thumbnail"  width="100%"
-                        height="60%">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title"><a href="Farasan_Islands.php" class="text-secondary">جزيرة فرسان</a></h4>
-                        <p class="card-text-center">جزيرة فرسان تتميز بمنظر طبيعي خلاب ورمالها البيضاء وتدرج ألوان مياهها الرائعة ويوجد بها تنوع بيولوجي الذي يجذب كثير من السياح</p>
-                        <a href="Farasan_Islands.php" class=" btn btn-outline-success">&numsp;عرض&numsp;</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4">
-                <div class="card my-3">
-                    <div class="card-thumbnail">
-                        <img src="assistances/images/amnh.jpg" class="img-fluid" alt="thumbnail"  width="100%"
-                        height="60%">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title"><a href="Amna.php" class="text-secondary">جزيرة أمنة</a></h4>
-                        <p class="card-text-center">جزيرة آمنة هي إحدى الجُزر السعودية ، وتتبع إدراياً لمحافظة جازان، وتفصل الجزيرة عن الساحل أو البر السعودي مسافة ما يزيد قليلاً عن سبعة أميالٍ بحرية، فيما تبعد حوالي خمسة عشر كيلومتراً، عن جنوب غرب ميناء مدينة جازان، وتبلغ مساحة الجزيرة حوالي 1.1كم².</p>
-                        <a href="Amna.php" class=" btn btn-outline-success">&numsp;عرض&numsp;</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4">
-                <div class="card my-3">
-                    <div class="card-thumbnail">
-                        <img src="assistances/images/amnh.jpg" class="img-fluid" alt="thumbnail"  width="100%"
-                        height="60%">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title"><a href="umqamari.php" class="text-secondary">جزيرة أم القماري</a></h4>
-                        <p class="card-text-center">تكثر النباتات في وسط الجزيرتين وأهمها الأراك والسّواد والصّبار والثندة والرغل التّي تكثر أيضا على السّواحل.
-وإضافة إلى طيور القماري المهاجرة والمقيمة, هناك أنواع كثيرة من الطّيور البحريّة والطّيور الشاطئيّة وعدد من الطّيور البرية مثل العقاب النّساري ومالك الحزين والبلشون الأبيض والقمري المطوّق الأفريقي. أما الحياة البحرية فتمتاز بتنوع هائل من الشّعاب المرجانيّة والحيوانات اللافقاريّة البحريّة.</p>
-                        <a href="umqamari.php" class=" btn btn-outline-success">&numsp;عرض&numsp;</a>
-                    </div>
-                </div>
-            </div>
-
-    <!-- Bootstrap 5 Cards in Grid -->
-
-
-
-        </div>
     </div>
-</section>
-<?php require('components/footre.php');?>
+    <main class="container">
+        <!-- Bootstrap 5 Cards in Grid -->
+        <section class="bg-light py-4 my-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 id="scrollspyHeading2"></h2>
+                    </div>
+                    <?php
+                    if($_SESSION["destination"] == "island")
+                    {
+                        require 'connect_database.php';
+                        $select_card_info = $connect_database->prepare
+                        ('
+                        SELECT i.ID ID , i.name name , ic.card_description card_description , ic.card_photo card_photo
+                        FROM island i , island_content ic
+                        WHERE i.ID = ic.island_id AND i.ID IS NOT NULL
+                        ');
+                        $select_card_info->execute();
+                        foreach($select_card_info as $print)
+                        {
+                            echo '
+                                <div class="col-md-6 col-lg-4">
+                                    <div class="card my-3">
+                                        <div class="card-thumbnail">
+                                            <img src="assistances/images/'.$print["card_photo"].'" class="img-fluid" alt="thumbnail" width="100%" height="100%">
+                                        </div>
+                                    <div class="card-body">
+                                    <h4 class="card-title">'.$print["name"].'</h4>
+                                    <p class="card-text-center">'.$print["card_description"].'</p>
+                                        <form method="POST" action="try2.php">
+                                        <button type="submit" name="card_island" class=" btn btn-outline-success" value="'.$print["ID"].'">عرض</button>
+                                        </form>
+                                    </div>
+                                    </div>
+                                </div>
+                            ';
+                        }
+                    }
+                    ?>
+
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php require('components/footre.php'); ?>
 </body>
+
 </html>

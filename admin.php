c444372f49964b52c5b2e3e45bb7f5f7a1673759
AhmedSaadounn <75604506+AhmedSaadounn@users.noreply.github.com>
2022-05-27 15:17:08 +0300

    add admin

--- admin.php
+++ admin.php
@@ -15,42 +15,35 @@
 <!-- col_1 -->
       <div class="feature col">
         <div class="feature-icon bg-white">
-        <img src="assistances/images/add.png"class="bd-placeholder-img rounded-circle" width="140" height="140"></img>
+        <img src="assistances/images/add.png"class="bd-placeholder-img rounded-circle" width="100" height="100"></img>
         </div>
-        <h2>الطلابات </h2>
-        <a href="#"class="btn btn-secondary">
-        عرض التفاصيل     
-         </a>
+        <h4> &numsp;الطلابات</h4>
+        <input  class="btn btn-secondary btn-sm" type="button" value="عرض التفاصيل"></input>
       </div>
 <!-- // col1-->
 <!-- col_2 -->
       <div class="feature col">
         <div class="feature-icon bg-white">
-        <img src="assistances/images/ad1.png"class="bd-placeholder-img rounded-circle" width="140" height="140"></img>
+        <img src="assistances/images/ad1.png"class="bd-placeholder-img rounded-circle" width="100" height="100"></img>
         </div>
-        <h2> المحتوى</h2>
-        <a href="#"class="btn btn-secondary">
-        عرض التفاصيل     
-         </a>
+        <h4> &numsp;المحتوى</h4>
+        <input  class="btn btn-secondary btn-sm" type="button" value="عرض التفاصيل"></input>
       </div>
       <!-- //col2 -->
       
       <!-- col_3 -->
       <div class="feature col">
         <div class="feature-icon bg-white">
-        <img src="assistances/images/ad.jpg"class="bd-placeholder-img rounded-circle" width="140" height="140"></img>
+        <img src="assistances/images/ad.jpg"class="bd-placeholder-img rounded-circle" width="100" height="100"></img>
         </div>
-        <h2>المستخدمين</h2>
-        <a href="#"class="btn btn-secondary">
-        عرض التفاصيل     
-         </a>
+        <h4>المستخدمين</h4>
+        <input  class="btn btn-secondary btn-sm" type="button" value="عرض التفاصيل"></input>
       </div>
       <!-- //col3 -->
     </div>
+    <hr>
   </div>
-  <hr>
 </main>
   <?php require('components/footre.php'); ?>
 </body>
-
 </html>
